dbccce1bf3aabe2a99c9ebf4362cf9fbfd19b988
chaixs <1113935182@qq.com>
2021-03-06 17:05:45 +0800

    外观模式

---
+++ src/main/java/com/chai/DesignPattern/facade/Client.java
@@ -0,0 +1,8 @@
+package com.chai.DesignPattern.facade;
+
+public class Client {
+    public static void main(String[] args) {
+        Facade f = new Facade();
+        f.method();
+    }
+}
---
+++ src/main/java/com/chai/DesignPattern/facade/SubSystem03.java
@@ -0,0 +1,7 @@
+package com.chai.DesignPattern.facade;
+
+public class SubSystem03 {
+    public void method3(){
+        System.out.println("子系统03的method3()被调用！");
+    }
+}
