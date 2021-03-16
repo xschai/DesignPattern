f55b0519bffaf054a10de6ce48bc169f8fbe2edb
chaixs <1113935182@qq.com>
2021-03-16 19:39:05 +0800

    责任链模式

---
+++ src/main/java/com/chai/DesignPattern/ChainofResponsibility/Client.java
@@ -0,0 +1,12 @@
+package com.chai.DesignPattern.ChainofResponsibility;
+
+public class Client {
+    public static void main(String[] args) {
+        //组装责任链
+        Handler handler1 = new ConcreteHandler1();
+        Handler handler2 = new ConcreteHandler2();
+        handler1.setNext(handler2);
+        //提交请求
+        handler1.handleRequest("two");
+    }
+}
---
+++ src/main/java/com/chai/DesignPattern/ChainofResponsibility/Handler.java
@@ -0,0 +1,14 @@
+package com.chai.DesignPattern.ChainofResponsibility;
+
+//抽象处理者角色
+public abstract class Handler {
+    private Handler next;
+    public void setNext(Handler next) {
+        this.next = next;
+    }
+    public Handler getNext() {
+        return next;
+    }
+    //处理请求的方法
+    public abstract void handleRequest(String request);
+}
